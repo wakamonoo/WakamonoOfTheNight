b0e668f1d7dc7ac47347771ecbf090012412dceb
wakamonoo <joven.serdanbataller21@gmail.com>
2025-07-25 23:25:32 +0800

    page routes

---
+++ src/app/skills/page.jsx
@@ -0,0 +1,11 @@
+// app/about/page.jsx
+'use client';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import BottomHalfCircleMenu from '@/components/buttons';
+
+export default function AboutPage() {
+  return (
+    <BottomHalfCircleMenu />
+  );
+}
--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -3,28 +3,46 @@ import { VscAccount } from "react-icons/vsc";
 import { RiRocket2Fill } from "react-icons/ri";
 import { CgWebsite } from "react-icons/cg";
 import { BsTelephoneFill } from "react-icons/bs";
+import Link from "next/link";
 
 export default function BottomHalfCircleMenu() {
   return (
     <div className="fixed bottom-0 left-1/2 -translate-x-1/2 z-50">
       <div className="bg-text rounded-t-full relative">
-        <div id="navLinks" className="left-1/2 -translate-x-1/2 text-secondary text-4xl w-64 h-35 relative">
+        <div
+          id="navLinks"
+          className="left-1/2 -translate-x-1/2 text-secondary text-4xl w-64 h-35 relative"
+        >
           <div>
-            <button className="absolute left-6 bottom-4 transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]">
-            <RiRocket2Fill />
-          </button>
-          <button className="absolute left-14 bottom-16 transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]">
-            <VscAccount />
-          </button>
-          <button className="absolute left-1/2 -translate-x-1/2 bottom-22 transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]">
-            <AiFillHome />
-          </button>
-          <button className="absolute right-14 bottom-16 transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]">
-            <CgWebsite />
-          </button>
-          <button className="absolute right-6 bottom-4 transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]">
-            <BsTelephoneFill />
-          </button>
+            <Link href="./skills">
+              <button className="absolute left-6 bottom-4 transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]">
+              <RiRocket2Fill />
+            </button>
+            </Link>
+
+            <Link href="./about">
+              <button className="absolute left-14 bottom-16 transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]">
+                <VscAccount />
+              </button>
+            </Link>
+
+            <Link href="/">
+              <button className="absolute left-1/2 -translate-x-1/2 bottom-22 transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]">
+              <AiFillHome />
+            </button>
+            </Link>
+
+            <Link href="./portfolio">
+              <button className="absolute right-14 bottom-16 transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]">
+              <CgWebsite />
+            </button>
+            </Link>
+
+            <Link href="./contact">
+              <button className="absolute right-6 bottom-4 transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]">
+              <BsTelephoneFill />
+            </button>
+            </Link>
           </div>
         </div>
         <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-28 h-14 bg-accent rounded-t-full transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]"></div>
