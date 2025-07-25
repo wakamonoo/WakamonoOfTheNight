4683421aab11c3c724b284624f19fc51d828c396
wakamonoo <joven.serdanbataller21@gmail.com>
2025-07-25 18:11:06 +0800

    push

--- src/components/signage.jsx
+++ src/components/signage.jsx
@@ -2,7 +2,7 @@ export default function Signage() {
   return(
     <div className="flex justify-center align-center w-full ">
       <div className="relative">
-        <p className="absolute text-center font-tall text-[4.5rem] text-bone opacity-10 leading-[4.3rem] whitespace-nowrap">WAKAMONO <br />OF THE NIGHT</p>
+        <p className="absolute text-center font-tall text-[4.5rem] text-panel opacity-40 leading-[4.3rem] whitespace-nowrap">WAKAMONO <br />OF THE NIGHT</p>
         <p className="relative text-center font-tall text-[4.5rem] text-transparent stroke-text pointer-events-none z-40 leading-[4.3rem] whitespace-nowrap">WAKAMONO<br />OF THE NIGHT</p>
       </div>
     </div>
