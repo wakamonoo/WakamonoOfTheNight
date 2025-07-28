d13655e3ebcf53ce4e9387ea31867cf22a47da90
wakamonoo <joven.serdanbataller21@gmail.com>
2025-07-28 13:18:51 +0800

    cards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,9 +19,6 @@ import {
   SiRender,
 } from "react-icons/si";
 import { FaGithub } from "react-icons/fa";
-import ScrollStack, {
-  ScrollStackItem,
-} from "@/blocks/Components/ScrollStack/ScrollStack";
 
 export default function Home() {
   return (
@@ -193,20 +190,7 @@ export default function Home() {
 
       <section id="portfolio" className="bg-second p-8">
         <h1 className="text-3xl mb-4 text-header">RECON MISSIONS ────</h1>
-        <ScrollStack>
-          <ScrollStackItem>
-            <h2>Card 1</h2>
-            <p>This is the first card in the stack</p>
-          </ScrollStackItem>
-          <ScrollStackItem>
-            <h2>Card 2</h2>
-            <p>This is the second card in the stack</p>
-          </ScrollStackItem>
-          <ScrollStackItem>
-            <h2>Card 3</h2>
-            <p>This is the third card in the stack</p>
-          </ScrollStackItem>
-        </ScrollStack>
+        
       </section>
     </main>
   );
