a91bccc703b340b922144b55969a7288bb94b826
wakamonoo <joven.serdanbataller21@gmail.com>
2025-07-29 20:46:03 +0800

    push

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -8,20 +8,20 @@ import Bell from "../assets/bell.png";
 export default function Intro() {
   return (
     <div className="flex justify-end">
-      <div className="relative z-50 p-4 right-[2rem] sm:right-[4rem] md:right-[10rem] lg:right-[10rem] xl:right-[15rem] top-[40rem]  md:top-[40rem] rounded-xl bg-panel w-70 sm:w-85 lg:w-110">
+      <div className="relative z-50 p-4 right-[5vw] lg:right-[5vw] xl:right-[12vw] top-[60vh] h-full rounded-xl bg-panel w-70 sm:w-85 lg:w-110 xl:w-150">
         <div className="absolute bottom-30 -left-8">
           <Image
             src={Bell}
             alt="bell"
-            className="w-16 sm:w-18 lg:w-24 h-auto overflow-auto -rotate-45"
+            className="w-16 sm:w-18 lg:w-24 xl:w-30 h-auto overflow-auto -rotate-45"
           />
         </div>
 
-        <h1 className="relative text-5xl sm:text-6xl lg:text-7xl text-header text-right">JOVEN BATALLER</h1>
-        <p className="text-right text-2xl sm:text-3xl lg:text-4xl text-normal">
-          <span className="text-2xl sm:text-3xl lg:text-4xl text-normal">──── </span>web developer
+        <h1 className="relative text-5xl sm:text-6xl lg:text-7xl xl:text-8xl text-header text-right">JOVEN BATALLER</h1>
+        <p className="text-right text-2xl sm:text-3xl lg:text-4xl xl:text-5xl text-normal">
+          <span className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl  text-normal">──── </span>web developer
         </p>
-        <div className="flex gap-2 text-4xl sm:text-5xl lg:text-6xl justify-center pt-2 text-normal overflow-visible">
+        <div className="flex gap-2 text-4xl sm:text-5xl lg:text-6xl xl:text-7xl justify-center pt-2 text-normal overflow-visible">
           <a href="https://www.facebook.com/joven.serdanbataller">
             <button className="transition-all duration-300 hover:scale-125 hover:text-[var(--color-accent)] active:scale-125 focus:text-[var(--color-accent)]">
               <FaFacebookF />
