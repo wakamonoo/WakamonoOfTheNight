e52f5336b7881caddab206fcff5af8ce61a2ce4c
wakamonoo <joven.serdanbataller21@gmail.com>
2025-07-30 13:06:10 +0800

    lg skills

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -55,6 +55,7 @@ export default function Home() {
         </div>
       </section>
 
+
       <section
         id="about"
         className="bg-second p-8 sm:p-15 md:p-24 lg:p-32 xl:px-72"
@@ -128,81 +129,82 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="skills" className="bg-second p-8 sm:p-15">
-        <h1 className="text-3xl sm:text-5xl mb-4 text-header">
+
+      <section id="skills" className="bg-second p-8 sm:p-15 md:p-24 lg:p-32 xl:px-72">
+        <h1 className="text-3xl sm:text-5xl md:text-6xl mb-4 text-header">
           ODM ARSENAL ────
         </h1>
         <div className="flex justify-center">
-          <div className="grid grid-cols-4 sm:grid-cols-6 gap-2">
+          <div className="grid grid-cols-4 sm:grid-cols-6 md:grid-cols-4 gap-2 md:gap-4">
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <SiHtml5 />
             </div>
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <SiCss3 />
             </div>
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <SiJavascript />
             </div>
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <SiTailwindcss />
             </div>
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <SiReact />
             </div>
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <SiNextdotjs />
             </div>
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <SiFirebase />
             </div>
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <SiKotlin />
             </div>
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <FaGithub />
             </div>
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <SiVercel />
             </div>
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <SiVite />
             </div>
             <div
               tabIndex="0"
-              className="p-2 text-5xl sm:text-7xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
             >
               <SiRender />
             </div>
