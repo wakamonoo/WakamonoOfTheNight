0a2d39f655373b70e44806e5b5fa6d1e688e43b7
wakamonoo <joven.serdanbataller21@gmail.com>
2025-07-31 14:08:36 +0800

    emailjs

--- src/sections/contactSection.jsx
+++ src/sections/contactSection.jsx
@@ -3,43 +3,81 @@ import { FaGithub } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_63ovony",
+        "template_008hedl",
+        form.current,
+        "u8-c71FS0yKlrua_p"
+      )
+      .then(
+        () => {
+          alert("Message Deployed");
+          form.current.reset();
+        },
+        (error) => {
+          alert("Deployment Error, Try Again");
+          console.error(error.text);
+        }
+      );
+  };
+
   return (
     <>
       <h1 className="text-3xl sm:text-5xl md:text-6xl mb-4 text-header md:mb-8 lg:mb-12 xl:mb-16">
         DISPATCH RESOLVE ────
       </h1>
       <div>
-        <div className="flex flex-col gap-2">
+        <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-2">
           <input
             placeholder="enter cadet name"
+            name="name"
+            required
             type="text"
             className="w-full border bg-panel p-2 rounded"
           />
           <input
             placeholder="your signal relay code (email)"
+            name="email"
+            required
             type="email"
             className="w-full border bg-panel p-2 rounded"
           />
           <input
             placeholder="mission intent (subject)"
+            name="title"
+            required
             type="text"
             className="w-full border bg-panel p-2 rounded"
           />
           <textarea
             placeholder="detail your intel"
+            name="message"
+            required
             type="text"
             className="w-full h-40 border bg-panel p-2 rounded"
           />
-        </div>
-        <button
-          className="p-2 sm:p-4 sm:text-xl md:text-2xl mt-4 bg-brand rounded shadow-2xl border border-accent transition-all duration-200
+          <div>
+            <button
+              type="submit"
+              className="p-2 sm:p-4 sm:text-xl md:text-2xl mt-4 bg-brand rounded shadow-2xl border border-accent transition-all duration-200
              hover:bg-[var(--color-panel)] hover:scale-110 
              active:scale-110 active:bg-[var(--color-panel)]"
-        >
-          ALERT HEADQUARTERS
-        </button>
+            >
+              ALERT HEADQUARTERS
+            </button>
+          </div>
+        </form>
+
         <p className="font-normal text-sm sm:text-base md:text-xl py-8 text-normal">
           alternate recon points:
         </p>
