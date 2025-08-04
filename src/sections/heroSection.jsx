a26e5c3752bd2d50ea3ac1ba3409b13e329ce5d9
wakamonoo <joven.serdanbataller21@gmail.com>
2025-08-04 13:11:16 +0800

    fixed titan head crop

--- src/sections/heroSection.jsx
+++ src/sections/heroSection.jsx
@@ -11,8 +11,8 @@ export default function Hero() {
   return (
     <>
       <NavMenu />
-      <div className="absolute top-15 left-1/2 transform -translate-x-1/2 w-[35rem] sm:w-[40rem] h-[20rem] sm:h-[25rem] md:w-[45rem] md:h-[30rem] lg:w-[50rem] lg:h-[30rem]xl:w-[50rem] xl:h-[30rem]">
-        <Image src={Titan} alt="titan" fill className="object-cover blur-xs" />
+      <div className="absolute top-8 left-1/2 transform -translate-x-1/2 w-[35rem] sm:w-[40rem] h-[20rem] sm:h-[25rem] md:w-[45rem] md:h-[30rem] lg:w-[50rem] lg:h-[30rem]xl:w-[50rem] xl:h-[30rem]">
+        <Image src={Titan} alt="titan" fill className="object-cover object-top blur-xs" />
       </div>
 
       <div
