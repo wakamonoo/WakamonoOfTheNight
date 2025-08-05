f8327ac444a0d7526d9b7358167017debda9a720
wakamonoo <joven.serdanbataller21@gmail.com>
2025-08-05 20:14:11 +0800

    guilty

--- src/sections/heroSection.jsx
+++ src/sections/heroSection.jsx
@@ -6,25 +6,18 @@ import NavMenu from "@/components/buttons";
 import Signage from "@/components/signage";
 import Intro from "@/components/intro";
 import Titan from "../assets/bertolo.png";
-import SmokeEffect from "@/components/smokeEffect";
 
 export default function Hero() {
   return (
     <>
       <NavMenu />
-      <div className="absolute top-8 left-1/2 transform -translate-x-1/2 w-[35rem] sm:w-[40rem] h-[20rem] sm:h-[25rem] md:w-[45rem] md:h-[30rem] lg:w-[50rem] lg:h-[30rem]xl:w-[50rem] xl:h-[30rem] z-10">
-        <Image
-          src={Titan}
-          alt="titan"
-          fill
-          className="object-cover object-top blur-xs"
-        />
+      <div className="absolute top-8 left-1/2 transform -translate-x-1/2 w-[35rem] sm:w-[40rem] h-[20rem] sm:h-[25rem] md:w-[45rem] md:h-[30rem] lg:w-[50rem] lg:h-[30rem]xl:w-[50rem] xl:h-[30rem]">
+        <Image src={Titan} alt="titan" fill className="object-cover object-top blur-xs" />
       </div>
-        <SmokeEffect />
 
       <div
         id="bg"
-        className="absolute bottom-0 left-0 w-full inset-0 top-45 sm:top-40 lg:top-28 z-20"
+        className="absolute bottom-0 left-0 w-full inset-0 top-45 sm:top-40 lg:top-28"
       >
         <Signage />
         <Image
@@ -36,7 +29,7 @@ export default function Hero() {
       <div id="meInfo">
         <div
           id="mePhoto"
-          className="absolute bottom-0 left-1/2 transform -translate-x-1/2 z-30
+          className="absolute bottom-0 left-1/2 transform -translate-x-1/2 z-10
              w-full h-[85vh] sm:h-[90vh] max-w-[450px] max-h-[850px] md:max-h-[1000px] lg:max-h-[1024px]
              flex justify-center items-end"
         >
