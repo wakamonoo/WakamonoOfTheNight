df98be3595d565e11371ae8dd049e71d16048983
wakamonoo <joven.serdanbataller21@gmail.com>
2025-08-08 12:29:28 +0800

    user message initialized

--- src/components/aibou.jsx
+++ src/components/aibou.jsx
@@ -1,9 +1,21 @@
 import { MdMessage } from "react-icons/md";
 import { useState } from "react";
 import { FiSend } from "react-icons/fi";
+import { FiUser } from "react-icons/fi"; 
 
 export default function Aibou() {
   const [isOpen, setIsOpen] = useState(false);
+  const [draftText, setDraftText] = useState("");
+  const [sentText, setSentText] = useState("");
+
+  function handleSent() {
+    if(draftText.trim !== "") {
+      setSentText(draftText);
+      setDraftText("");
+    }
+  }
+
+
 
   return (
     <>
@@ -15,17 +27,27 @@ export default function Aibou() {
       </div>
 
       {isOpen && (
-        <div className="fixed bottom-22 right-4 z-100 bg-army shadow-2xl w-80 h-110 rounded-2xl overflow-hidden">
+        <div className="fixed flex flex-col bottom-22 right-4 z-100 bg-army shadow-2xl w-80 h-110 rounded-2xl overflow-hidden">
           <div className="p-2 text-xl text-header flex justify-center items-center gap-4 w-full h-8 bg-panel">
             <MdMessage className="font-heading" />
             <p className=" font-heading">AIbou</p>
           </div>
 
-          <div className="flex-1 p-2 overflow-y-auto"></div>
+          <div className="flex-1 p-2 overflow-y-auto">
+            <div className="flex items-center gap-2 justify-end pl-4">
+              <p className="bg-brand p-2 text-normal rounded-2xl">{sentText}</p>
+              <FiUser className="text-4xl bg-brand rounded-2xl p-2"/>
+            </div>
+          </div>
 
           <div className="flex justify-between gap-2 items-center p-2">
-            <textarea className="w-full h-12 bg-brand p-2 rounded-md" />
-            <FiSend className="text-5xl bg-brand p-2 rounded-md" />
+            <textarea
+              className="w-full h-12 bg-brand p-2 rounded-md"
+              placeholder="what do you want to ask?"
+              value={draftText}
+              onChange={(e) => setDraftText(e.target.value)}
+            />
+            <FiSend onClick={handleSent} className="text-5xl text-normal bg-brand p-2 rounded-md" />
           </div>
         </div>
       )}
