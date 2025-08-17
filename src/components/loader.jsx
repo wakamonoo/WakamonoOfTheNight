e6acc3b1248ec8aa722ccc1bb87739043842b233
wakamonoo <joven.serdanbataller21@gmail.com>
2025-08-17 21:44:20 +0800

    logo update

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -9,7 +9,7 @@ export default function Loader() {
         alt="wakamono-logo"
         className="absolute object-cover w-36 sm:w-50 lg:w-60 xl:70 h-auto animate-[center-left_1s_ease-in-out_forwards] [animation-delay:500ms]"
       />
-      <h1 className="absolute font-tall text-6xl sm:text-7xl lg:text-8xl xl:text-9xl opacity-0 text-black ml-24 sm:ml-32 lg:ml-42 xl:ml-50 animate-[wkmn-fade_1s_ease-in-out_forwards] [animation-delay:700ms]">WKMN</h1>
+      <h1 className="absolute font-tall text-6xl sm:text-7xl lg:text-8xl xl:text-9xl opacity-0 text-black ml-32 sm:ml-40 lg:ml-56 xl:ml-60 animate-[wkmn-fade_1s_ease-in-out_forwards] [animation-delay:700ms]">WKMN</h1>
     </div>
   );
 }
