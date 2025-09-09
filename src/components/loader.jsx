c7e55df3933cc7c1d6e8042c11d22e923f6fe827
wakamonoo <joven.serdanbataller21@gmail.com>
2025-09-09 13:11:19 +0800

    fade in

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Logo from "../assets/logo.png";
 import Image from "next/image";
 
 export default function Loader({ onFinish }) {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 500);
+  }, []);
   return (
     <div className="fixed inset-0 z-100 backdrop-blur-2xl flex justify-center items-center w-screen h-screen ">
-      <Image
-        src={Logo}
-        alt="wakamono-logo"
-        priority
-        className="absolute object-cover w-36 sm:w-50 lg:w-60 xl:70 h-auto animate-[center-left_1s_ease-in-out_forwards] [animation-delay:1000ms]"
-      />
-      <h1
-        onAnimationEnd={onFinish}
-        className="absolute font-tall text-6xl sm:text-7xl lg:text-8xl xl:text-9xl opacity-0 text-black ml-32 sm:ml-40 lg:ml-56 xl:ml-60 animate-[wkmn-fade_1s_ease-in-out_forwards] [animation-delay:1200ms]"
-      >
-        WKMN
-      </h1>
+      {show && (
+        <>
+          <Image
+            src={Logo}
+            alt="wakamono-logo"
+            priority
+            className="absolute object-cover w-36 sm:w-50 lg:w-60 xl:70 h-auto animate-[center-left_1s_ease-in-out_forwards] [animation-delay:1000ms]"
+          />
+          <h1
+            onAnimationEnd={onFinish}
+            className="absolute font-tall text-6xl sm:text-7xl lg:text-8xl xl:text-9xl opacity-0 text-black ml-32 sm:ml-40 lg:ml-56 xl:ml-60 animate-[wkmn-fade_1s_ease-in-out_forwards] [animation-delay:1200ms]"
+          >
+            WKMN
+          </h1>
+        </>
+      )}
     </div>
   );
 }
