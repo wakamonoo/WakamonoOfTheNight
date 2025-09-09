d9c1ab6cd251bdac1aa76c4f04c9da8f6c08f3eb
wakamonoo <joven.serdanbataller21@gmail.com>
2025-09-09 09:19:49 +0800

    organized skills section

--- src/sections/skillSection.jsx
+++ src/sections/skillSection.jsx
@@ -26,115 +26,173 @@ export default function Skills() {
       <h1 className="text-3xl sm:text-5xl md:text-6xl mb-4 text-header md:mb-8 lg:mb-12 xl:mb-16">
         ODM ARSENAL ────
       </h1>
-      <div className="flex justify-center">
-        <div className="grid grid-cols-4 sm:grid-cols-6 md:grid-cols-4 2xl:grid-cols-6 gap-2 md:gap-4">
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiHtml5 />
+      <div className="flex flex-col lg:grid lg:grid-cols-2 gap-4 justify-center mx-auto">
+        <div>
+          <h2 className="w-full text-2xl sm:text-3xl md:text-4xl p-2 bg-brand rounded text-header mb-4 mt-4">
+            Front-End Development
+          </h2>
+          <p className="text-normal text-base sm:text-xl md:text-2xl font-normal leading-4.5 sm:leading-6 md:leading-8 lg:leading-8.5 mb-4 md:mb-8">
+            Building user interfaces and interactive designs for web apps using
+            modern frameworks and styling tools.
+          </p>
+          <div className="flex flex-wrap gap-2">
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiHtml5 />
+            </div>
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiCss3 />
+            </div>
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiJavascript />
+            </div>
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiTailwindcss />
+            </div>
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiReact />
+            </div>
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiNextdotjs />
+            </div>
           </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiCss3 />
+        </div>
+
+        <div>
+          <h2 className="w-full text-2xl sm:text-3xl md:text-4xl p-2 bg-brand rounded text-header mb-4 mt-4">
+            Back-End Development
+          </h2>
+          <p className="text-normal text-base sm:text-xl md:text-2xl font-normal leading-4.5 sm:leading-6 md:leading-8 lg:leading-8.5 mb-4 md:mb-8">
+            Handling server-side logic, APIs, and data processing to make
+            applications functional and efficient.
+          </p>
+          <div className="flex flex-wrap gap-2">
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiExpress />
+            </div>
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiNodedotjs />
+            </div>
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiPhp />
+            </div>
           </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiJavascript />
+        </div>
+
+        <div>
+          <h2 className="w-full text-2xl sm:text-3xl md:text-4xl p-2 bg-brand rounded text-header mb-4 mt-4">
+            Database & Authentication
+          </h2>
+          <p className="text-normal text-base sm:text-xl md:text-2xl font-normal leading-4.5 sm:leading-6 md:leading-8 lg:leading-8.5 mb-4 md:mb-8">
+            Managing data storage, retrieval, and user authentication to keep
+            information secure and accessible.
+          </p>
+          <div className="flex flex-wrap gap-2">
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiFirebase />
+            </div>
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiMysql />
+            </div>
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiMongodb />
+            </div>
           </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)]"
-          >
-            <SiTailwindcss />
+        </div>
+
+        <div>
+          <h2 className="w-full text-2xl sm:text-3xl md:text-4xl p-2 bg-brand rounded text-header mb-4 mt-4">
+            Tools & Platforms
+          </h2>
+          <p className="text-normal text-base sm:text-xl md:text-2xl font-normal leading-4.5 sm:leading-6 md:leading-8 lg:leading-8.5 mb-4 md:mb-8">
+            Using deployment tools, version control, and cloud platforms to
+            build, host, and manage applications.
+          </p>
+          <div className="flex flex-wrap gap-2">
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <FaGithub />
+            </div>
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiVercel />
+            </div>
+
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiSocketdotio />
+            </div>
+
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiRender />
+            </div>
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiCloudinary />
+            </div>
           </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiReact />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiNextdotjs />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiFirebase />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiKotlin />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <FaGithub />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiVercel />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiPhp />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiMysql />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiSocketdotio />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiMongodb />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiExpress />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiNodedotjs />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiRender />
-          </div>
-          <div
-            tabIndex="0"
-            className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer"
-          >
-            <SiCloudinary />
+        </div>
+        <div>
+          <h2 className="w-full text-2xl sm:text-3xl md:text-4xl p-2 bg-brand rounded text-header mb-4 mt-4">
+            Mobile App Development
+          </h2>
+          <p className="text-normal text-base sm:text-xl md:text-2xl font-normal leading-4.5 sm:leading-6 md:leading-8 lg:leading-8.5 mb-4 md:mb-8">
+            Creating cross-platform and native mobile applications with
+            optimized performance and modern features.
+          </p>
+          <div className="flex flex-wrap gap-2">
+            <div
+              tabIndex="0"
+              className="p-2 md:p-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-normal bg-brand rounded transition-all duration-300 hover:scale-110 hover:text-[var(--color-accent)] active:scale-110 focus:text-[var(--color-accent)] cursor-pointer w-fit h-fit"
+            >
+              <SiKotlin />
+            </div>
           </div>
         </div>
       </div>
